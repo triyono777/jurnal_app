678c4058cdf01114148d0f8b770e1600cd450045
Triyono <yono.tri@gmail.com>
2022-06-25 15:11:04 +0700

    add form

---
+++ lib/screen/add_form_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/item_form_widget.dart';
+import '../data/data_jurnal.dart';
+
+class AddFormScreen extends StatefulWidget {
+  const AddFormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddFormScreen> createState() => _AddFormScreenState();
+}
+
+class _AddFormScreenState extends State<AddFormScreen> {
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController deskripsiController = TextEditingController();
+  final TextEditingController waktuController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Form'),
+      ),
+      body: Column(
+        children: [
+          ItemFormWidget(controller: namaController, labelText: 'Nama Jurnal'),
+          ItemFormWidget(
+            controller: deskripsiController,
+            labelText: 'Deskripsi Jurnal',
+          ),
+          ItemFormWidget(
+            controller: waktuController,
+            labelText: 'Waktu',
+          ),
+          ElevatedButton(
+            onPressed: () {
+              listJurnal.add(
+                {
+                  "nama_jurnal": namaController.text,
+                  "deskripsi_jurnal": deskripsiController.text,
+                  "waktu": waktuController.text
+                },
+              );
+              Navigator.of(context).pop(true);
+            },
+            child: Text('Simpan'),
+          ),
+        ],
+      ),
+    );
+  }
+}
